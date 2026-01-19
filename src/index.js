50ff5930e8d9b3709e22397735e3709384623bac
amitsingh2904 <amit.singh29004@gmail.com>
2026-01-19 16:10:23 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,34 @@
+
+import connectDB from "./db/index.js";
+import  dotenv from "dotenv" ;
+
+dotenv.config({
+  path : "./env"
+})
+
+
+
+  connectDB()
+
+
+
+  //import  express from "express"
+  // const app =  express()
+  // ( async() => {
+  //   try{
+  //     mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME} `)
+  //     app.on("error " , (error) => {
+  //       console.log("err" , error);
+  //       throw error
+  //     })
+  //     app.listen(process.env.PORT , () => {
+  //         console.log(`App is listening on port ${process.env.PORT}`);
+  //     })
+
+  //   }
+  //   catch (error) {
+  //     console.log("Error : " , error )
+  //     throw err
+  //   }
+
+  // })()
